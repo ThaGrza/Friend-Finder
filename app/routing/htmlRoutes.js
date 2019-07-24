a66e40bd3e97ffc3b70a2c093de4994dee19c59d
Andrew Griswold <andrewgriswold7@gmail.com>
2019-07-23 20:31:40 -0500

    Fixed file layout / server

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,12 +6,12 @@ module.exports = function (app) {
    
     // HTML GET Requests
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
 
    
     // Redirects to home page
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
     });
 };
